2aaa986ee69d1ac9edf0e8e9a1bb6ec0228b1e36
EricXia <xch@raxch.cn>
2022-03-07 13:35:29 +0800

    refactor: 将原来的页脚替换为封装后的页脚

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -131,20 +131,7 @@
     </v-main>
 
     <!--  页脚 -->
-    <v-footer app>
-      <span>&copy; 2019-{{ new Date().getFullYear() }}</span>
-      <v-spacer></v-spacer>
-      <a
-        target="_blank"
-        href="http://www.beian.miit.gov.cn/"
-        style="text-decoration: none"
-      >
-        <img
-          src="https://s1.ax1x.com/2020/07/02/NqYHbQ.png"
-          style="float: left"
-        />浙ICP备2021029370号-1
-      </a>
-    </v-footer>
+    <HonorFooter/>
   </v-app>
 </template>
 
