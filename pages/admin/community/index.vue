88d962863458cd94a0aee3a9fbf2afec6625df1f
EricXia <xch@raxch.cn>
2022-03-08 00:29:13 +0800

    feat: 共同体管理界面双击详情

--- pages/admin/community/index.vue
+++ pages/admin/community/index.vue
@@ -1,31 +1,45 @@
 <template>
   <v-row>
     <v-col cols="12">
-      <v-sheet class="mx-auto mb-4" elevation="8">
-        <v-data-table
-          :headers="headers"
-          :items="communities.content"
-          class="elevation-1"
-        >
-          <template #[`item.createDate`]="{item}">
-            {{ item.createDate|formatDate }}
-          </template>
-          <template #[`item.state`]="{ item }">
-            <v-chip v-if="item.state===1" color="green" dark> 可见</v-chip>
-            <v-chip v-else-if="item.state===0" color="grey" dark> 待通过</v-chip>
-            <v-chip v-else color="orange" dark>隐藏</v-chip>
-          </template>
-          <template #[`item.actions`]="">
-            <v-icon small class="mr-2"> mdi-pencil</v-icon>
-            <v-icon small>
-              mdi-delete
-            </v-icon>
-          </template>
-          <template #[`item.type`]="{item}">
-            {{ item.type ? '通知' : '文章' }}
-          </template>
-        </v-data-table>
-      </v-sheet>
+      <v-data-table
+        :headers="headers"
+        :items="communities.content"
+        class="elevation-2"
+        @click:row="clickRow"
+      >
+        <template #[`item.createDate`]="{item}">
+          {{ item.createDate|formatDate }}
+        </template>
+        <template #[`item.state`]="{ item }">
+          <v-chip v-if="item.state===1" color="green" dark> 可见</v-chip>
+          <v-chip v-else-if="item.state===0" color="grey" dark> 待通过</v-chip>
+          <v-chip v-else color="orange" dark>隐藏</v-chip>
+        </template>
+        <template #[`item.actions`]="">
+          <v-icon small class="mr-2"> mdi-pencil</v-icon>
+          <v-icon small>
+            mdi-delete
+          </v-icon>
+        </template>
+        <template #[`item.type`]="{item}">
+          {{ item.type ? '通知' : '文章' }}
+        </template>
+      </v-data-table>
+    </v-col>
+    <v-col cols="auto">
+      <v-dialog v-model="communityDialog" max-width="600">
+        <v-card>
+          <v-toolbar dense>
+            <v-toolbar-title>共同体信息</v-toolbar-title>
+          </v-toolbar>
+          <v-card-title>
+            {{ clickedItem ? clickedItem.title : '' }}
+          </v-card-title>
+          <v-card-text>
+
+          </v-card-text>
+        </v-card>
+      </v-dialog>
     </v-col>
   </v-row>
 </template>
@@ -37,17 +51,13 @@ import {Community, Page} from "~/plugins";
 
 interface Data {
   communities: Page<Community> | null
-  headers:DataTableHeader[]
+  headers: DataTableHeader[]
+  communityDialog: boolean
+  clickedItem: Community | null
 }
 
 export default Vue.extend({
   name: "AdminCommunity",
-  filters: {
-    formatDate(value: number) {
-      if (!value) return ''
-      return new Date(value).toLocaleString()
-    }
-  },
   layout: "admin",
   async asyncData({$axios}): Promise<object | void> {
     const {data} = await $axios.get("/api/community", {
@@ -63,33 +73,46 @@ export default Vue.extend({
   data(): Data {
     return {
       communities: null,
-      headers:[
+      communityDialog: false,
+      headers: [
         {
-          text:'状态',
-          value:'state'
+          text: '状态',
+          value: 'state'
         },
         {
-          text:'标题',
-          value:'title',
+          text: '标题',
+          value: 'title',
           cellClass: 'text-caption'
         },
         {
-          text:'创建者',
-          value:'user.name'
+          text: '创建者',
+          value: 'user.name'
         },
         {
-          text:'类型',
-          value:'type.name'
+          text: '类型',
+          value: 'type.name'
         },
         {
-          text:'创建时间',
-          value:'createDate'
+          text: '创建时间',
+          value: 'createDate'
         },
         {
-          text:'操作',
-          value:'actions'
+          text: '操作',
+          value: 'actions'
         }
-      ]
+      ],
+      clickedItem: null
+    }
+  },
+  head() {
+    return {
+      title: "共同体列表"
+    }
+  },
+  methods: {
+    clickRow(item: Community | null) {
+      this.clickedItem = item
+      this.communityDialog = true
     }
   }
 })
