f598bfa89763dabb63d411403facc718cac31a59
EricXia <xch@raxch.cn>
2022-03-08 00:30:01 +0800

    refactor: 注册全局的filter formatDate

--- plugins/components.ts
+++ plugins/components.ts
@@ -2,3 +2,16 @@ import Vue from 'vue'
 import Footer from '@/components/Footer.vue'
 
 Vue.component('HonorFooter', Footer)
+
+function formatDate(value: number) {
+  if (!value) return ''
+  return new Date(value).toLocaleString()
+}
+
+function formatStr(text: string, len: number) {
+  if (text.length < len) return text
+  else return text.substr(0, len) + '...'
+}
+
+Vue.filter('formatDate', formatDate)
+Vue.filter('formatStr', formatStr)
