1b819a49cb60e40fc8763d2813d564662c7953b9
EricCHXia <52739449+ericCHxia@users.noreply.github.com>
2023-03-09 11:01:11 +0800

    fix(admin/config): 修改了学期设置错误的BUG

--- pages/admin/config.vue
+++ pages/admin/config.vue
@@ -4,16 +4,8 @@
     <v-card-text>
       <v-row>
         <v-col md="6">
-          <v-select
-            v-model="config.semester"
-            :items="semesters"
-            item-text="text"
-            item-value="value"
-            label="学期设置"
-            outlined
-            class="activate"
-            :append-outer-icon="config.semester!==oldConfig.semester?'mdi-check-all':''"
-          >
+          <v-select v-model="config.semester" :items="semesters" item-text="text" item-value="value" label="学期设置" outlined
+            class="activate" :append-outer-icon="config.semester !== oldConfig.semester ? 'mdi-check-all' : ''">
           </v-select>
         </v-col>
       </v-row>
@@ -26,53 +18,43 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import Component from 'vue-class-component'
 import { HonorConfig } from '~/src'
 
-interface Data {
-  config: HonorConfig,
-  oldConfig: HonorConfig,
-  semesters: {
-    text: string
-    value: number
-  }[]
-}
-
-export default Vue.extend({
+@Component({
   name: 'Config',
   layout: 'admin',
   async asyncData({ $axios }) {
     const configResonpse = await $axios.get('/api/config')
     const config = configResonpse.data.data
-    return { config }
-  },
-  data(): Data {
-    return {
-      config: {},
-      semesters: [
-      ],
-      oldConfig: {},
-    }
-  },
-  mounted() {
-    for(let i=3;i<=60;i++){
-      this.semesters.push({
-        text: `${2019+Math.floor((i-1)/2)}-${2020+Math.floor((i-1)/2)} 第${i%2===0?'二':'一'}学期`,
+    const oldConfig = JSON.parse(JSON.stringify(config))
+    const semesters = []
+    for (let i = 3; i <= 60; i++) {
+      semesters.push({
+        text: `${2019 + Math.floor(i/ 2)}-${2020 + Math.floor(i/ 2)} 第${i % 2 === 0 ? '一' : '二'}学期`,
         value: i,
       })
     }
-    this.oldConfig = JSON.parse(JSON.stringify(this.config))
-  },
-  methods: {
-    async saveConfig() {
-      try {
-        await this.$axios.put('/api/config', this.config)
-        this.$toast.success('保存成功')
-        this.oldConfig = JSON.parse(JSON.stringify(this.config))
-        this.$nuxt.refresh()
-      } catch (e) {
-        this.$toast.error('保存失败')
-      }
-    },
+    return { config, oldConfig, semesters }
   },
 })
+export default class extends Vue {
+  config = {} as HonorConfig
+  oldConfig = {} as HonorConfig
+  semesters = [] as {
+    text: string
+    value: number
+  }[]
+
+  async saveConfig() {
+    try {
+      await this.$axios.put('/api/config', this.config)
+      this.$toast.success('保存成功')
+      this.oldConfig = JSON.parse(JSON.stringify(this.config))
+      this.$nuxt.refresh()
+    } catch (e) {
+      this.$toast.error('保存失败')
+    }
+  }
+}
 </script>
